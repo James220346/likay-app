46edc02949c018dbf248153c9ce93ad75d7e9558
Sontaya <isusjamess@gmail.com>
2026-06-13 14:17:48 +0700

    เพิ่มไฟล์ +html บังคับไอคอนมือถือให้เป็นรูปลิเก

---
+++ src/app/+html.tsx
@@ -0,0 +1,25 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="th">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        
+        {/* 🚀 คำสั่งประกาศิต: บังคับให้มือถือทุกค่ายดึงรูปนี้ไปทำเป็นไอคอนแอป */}
+        <link rel="apple-touch-icon" href="/likay_logo.png" />
+        <link rel="icon" type="image/png" href="/likay_logo.png" />
+        
+        {/* ตั้งค่าให้หน้าแอปดูเป็นแอปมือถือจริงๆ (ซ่อนแถบเบราว์เซอร์) */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="บัญชีลิเก" />
+
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
